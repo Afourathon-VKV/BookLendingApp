e744f10ba5142a9bfde8f5061d67944863244284
KalyanRam1234 <kalyanram@pop-os.localdomain>
2023-07-04 21:11:34 +0530

    Modified external api routes

--- src/main/java/com/Dockerates/BookLending/Constants.java
+++ src/main/java/com/Dockerates/BookLending/Constants.java
@@ -1,7 +1,7 @@
 package com.Dockerates.BookLending;
 
 public class Constants {
-    public static String BookUrl="http://52.66.255.140:80/";
+    public static String BookUrl="http://3.110.217.62:80";
     public static String StudentUrl="http://13.127.251.149:80/";
 
 
