5e8cff006fbcc39f3aa490029e0c9f3f168c9c1e
LieutPaul <Vikas.Kalyanapuram@iiitb.ac.in>
2023-07-09 16:22:33 +0530

    Adding logging for booklending,user service and booklending controller

--- src/main/java/com/Dockerates/BookLending/Service/UserServiceImpl.java
+++ src/main/java/com/Dockerates/BookLending/Service/UserServiceImpl.java
@@ -10,6 +10,8 @@ import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -24,6 +26,7 @@ public class UserServiceImpl implements UserService {
     private final PasswordEncoder passwordEncoder;
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
+    private final Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
 
     @Override
     public String signup(User user) throws UserDuplicateEmailException {
@@ -38,8 +41,10 @@ public class UserServiceImpl implements UserService {
                 .role(user.getRole())
                 .build();
         try {
+            logger.info(String.format("Added User with email %s.", user.getEmail()));
             userRepository.save(newUser);
         } catch (DataIntegrityViolationException e) {
+            logger.error(String.format("Could not add User of email %s as account with that email exists.",user.getEmail()));
             throw new UserDuplicateEmailException("Email already exists");
         }
         return jwtService.generateToken(newUser);
@@ -47,7 +52,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public String login(User user) throws UserNotFoundException, UserWrongPasswordException {
-        System.out.println("Authenticating user: " + user.getEmail());
+        logger.info("Authenticating user: " + user.getEmail());
         try {
             authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(
@@ -56,15 +61,17 @@ public class UserServiceImpl implements UserService {
                     )
             );
         } catch (AuthenticationException e) {
+            logger.error(String.format("User with email %s tried logging in with the wrong password.",user.getEmail()));
             throw new UserWrongPasswordException("Wrong password");
         }
-        System.out.println("Authentication successful: " + user.getEmail());
+        logger.info("Authentication successful: " + user.getEmail());
         User newUser = userRepository.findByEmail(user.getEmail()).orElseThrow(() -> new UserNotFoundException("User not found"));
         return jwtService.generateToken(newUser);
     }
 
     @Override
     public List<User> getUsers(Role role){
+        logger.info("Fetching all users of type " + role.toString());
         return this.userRepository.findByRole(role);
     }
 
@@ -84,16 +91,20 @@ public class UserServiceImpl implements UserService {
             newUser.setName(user.getName());
             userRepository.save(newUser);
         } catch (DataIntegrityViolationException e) {
+            logger.error("Could not add librarian with email " + user.getEmail() + "as they are already added.");
             throw new UserDuplicateEmailException("Email already exists");
         }
+        logger.info("Added librarian with email" + user.getEmail());
         return newUser;
     }
 
     @Override
     public String deleteLibrarian(String email) throws UserNotFoundException{
         if(this.userRepository.deleteByEmail(email).equals("0")){
+            logger.error("Could not delete librarian with email " + email + "As librarian does not exist");
             throw new UserNotFoundException("This email doesn't exist");
         }
+        logger.info("Deleted librarian with email " + email);
         return this.userRepository.deleteByEmail(email);
     }
 }
